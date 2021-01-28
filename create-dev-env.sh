e70d13dd9c303f7dd63e6357019b541f42c8e185
Matthew Jones <m.t.j.rider@icloud.com>
2021-01-27 21:58:10 -0700

    reducing jupyterlab version to accommodate nvdashboard

--- create-dev-env.sh
+++ create-dev-env.sh
@@ -2,10 +2,14 @@
 
 CC=$1
 CXX=$2
+CONDA=$3
+
+source ${CONDA}/etc/profile.d/conda.sh
 
 create_dev_env() {
     conda env remove --name dev
     CC=${CC} CXX=${CXX} conda env create --name dev --file conda/environments/dev.yml
+    conda activate dev && jupyter labextension install jupyterlab-nvdashboard
 }
 
-create_dev_env $@
+create_dev_env
