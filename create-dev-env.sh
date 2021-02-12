a13577eecd093b4a2c686d4739c41580f16363e2
Matthew Jones <m.t.j.rider@icloud.com>
2021-02-11 18:38:17 -0700

    adding flags for horovod

--- create-dev-env.sh
+++ create-dev-env.sh
@@ -12,8 +12,8 @@ create_dev_env() {
     conda env remove --name dev
     if [[ ${TARGET_OS} = "macOS" ]]
     then
-        COMMAND="CC=${CC} CXX=${CXX} conda env create --name dev --file ${DIR}/conda/environments/dev-macos.yml"
-        bash -c "echo ${COMMAND} && HOROVOD_WITH_TENSORFLOW=1 HOROVOD_WITH_PYTORCH=1 ${COMMAND}"
+        COMMAND="HOROVOD_WITH_TENSORFLOW=1 HOROVOD_WITH_PYTORCH=1 CC=${CC} CXX=${CXX} conda env create --name dev --file ${DIR}/conda/environments/dev-macos.yml"
+        bash -c "echo ${COMMAND} && ${COMMAND}"
     elif [[ ${TARGET_OS} = "linux" ]]
     then
         COMMAND="CC=${CC} CXX=${CXX} conda env create --name dev --file ${DIR}/conda/environments/dev-linux.yml"
