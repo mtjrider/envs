bf58c04afa0f6735b932a0923d7fc41bf654200f
Matthew Jones <m.t.j.rider@icloud.com>
2021-02-11 23:56:41 -0700

    update for enabling horovod

--- create-dev-env.sh
+++ create-dev-env.sh
@@ -9,6 +9,7 @@ CONDA=$4
 source ${CONDA}/etc/profile.d/conda.sh
 
 create_dev_env() {
+    conda deactivate
     conda env remove --name dev
     if [[ ${TARGET_OS} = "macOS" ]]
     then
@@ -16,11 +17,13 @@ create_dev_env() {
         bash -c "echo ${COMMAND} && ${COMMAND}"
     elif [[ ${TARGET_OS} = "linux" ]]
     then
-        COMMAND="HOROVOD_WITH_TENSORFLOW=1 HOROVOD_WITH_PYTORCH=1 HOROVOD_GPU_OPERATIONS=NCCL HOROVOD_NCCL_LINK=SHARED CC=${CC} CXX=${CXX} conda env create --name dev --file ${DIR}/conda/environments/dev-linux.yml"
+        COMMAND="CC=${CC} CXX=${CXX} conda env create --name dev --file ${DIR}/conda/environments/dev-linux.yml"
         bash -c "echo ${COMMAND} && ${COMMAND}"
-        conda activate dev && jupyter labextension install jupyterlab-nvdashboard
+        conda activate dev && \
+            jupyter labextension install jupyterlab-nvdashboard && \
+            HOROVOD_WITH_TENSORFLOW=1 HOROVOD_WITH_PYTORCH=1 HOROVOD_GPU_OPERATIONS=NCCL pip install git+https://github.com/horovod/horovod.git@master
     else
-        exit -1
+        exit 1
     fi
 }
 
