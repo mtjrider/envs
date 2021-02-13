c21dee97ca90bcb361f1dec5480e421a7e3bc94f
Matthew Jones <m.t.j.rider@icloud.com>
2021-02-13 13:15:18 -0700

    correcting horovod install, adding it back into the installation script

--- create-dev-env.sh
+++ create-dev-env.sh
@@ -20,7 +20,8 @@ create_dev_env() {
         COMMAND="CC=${CC} CXX=${CXX} conda env create --name dev --file ${DIR}/conda/environments/dev-linux.yml"
         bash -c "echo ${COMMAND} && ${COMMAND}"
         conda activate dev && \
-            jupyter labextension install jupyterlab-nvdashboard
+            jupyter labextension install jupyterlab-nvdashboard && \
+            HOROVOD_WITH_TENSORFLOW=1 HOROVOD_WITH_PYTORCH=1 HOROVOD_GPU_OPERATIONS=NCCL pip install git+https://github.com/horovod/horovod.git@v0.21.2
     else
         exit 1
     fi
